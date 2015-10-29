3d759fa4631211188d0bc49b1d1502498caa5a7f
sibosop <brian@eastshore.com>
2015-10-29 15:04:44 -0700

    converted to SPI

--- sketches/LedMatrix/LedMatrix.ino
+++ sketches/LedMatrix/LedMatrix.ino
@@ -1,9 +1,12 @@
+#include <SPI.h>
 
 
+#define MISO 12
+#define MOSI 11
+#define SPICLK 13
 
-
-const int dataPin  = 2;
-const int clockPin = 3;
+const int dataPin  = MOSI;
+const int clockPin = SPICLK;
 const int latchPin = 4;
 const int resetPin = 5;
 
@@ -11,9 +14,10 @@ const int resetPin = 5;
 void setup() {
   //set pins to output because they are addressed in the main loop
   pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT); 
-  pinMode(clockPin, OUTPUT);
+  pinMode(MOSI, OUTPUT); 
+  pinMode(SPICLK, OUTPUT);
   pinMode(resetPin, OUTPUT);
+  pinMode(MISO,INPUT);
   Serial.begin(9600);
   Serial.println("reset");
   digitalWrite(latchPin, LOW);
@@ -21,32 +25,57 @@ void setup() {
   delay(10);
   digitalWrite(resetPin,1);
   digitalWrite(latchPin, HIGH);
+  SPI.begin();
 }
+
+struct RowVal {
+  byte vals[3];
+  
+  RowVal() 
+  {
+    memset(vals,0,3);
+  }
+    
+  void set(byte r, byte gp, byte rp)
+  {
+    memset(vals,0,3);
+    bitWrite(vals[2],r,1);
+    bitWrite(vals[1],gp,1);
+    bitWrite(vals[0],rp,1);
+  }
+  void print()
+  {
+    Serial.print("row:"); Serial.print(vals[2]);
+    Serial.print(" green: "); Serial.print(vals[1]);
+    Serial.print(" red: "); Serial.println(vals[0]);
+  }
+};
 
 void loop() {
   
   for ( int i = 0; i < 8; i++ )
   {
     
-    byte rows = 0;
+    RowVal rv;
+    
 
     // turn off the output so the pins don't light up
-    // while you're shifting bits:
-    
-
+    // while you're shifting bits
     // turn on the next highest bit in bitsToSend:
-    
-    
-    bitWrite(rows, i, 1);
     for ( int j = 0; j < 8; ++j )
     {
-      byte color = 0;
-      bitWrite(color,j,1);
+      rv.set(i,j,7-j);
+      //rv.print();
       digitalWrite(latchPin, LOW);
-      digitalWrite(clockPin, LOW);
-      shiftOut(dataPin, clockPin, LSBFIRST, color);
-      shiftOut(dataPin, clockPin, MSBFIRST, color);
-      shiftOut(dataPin, clockPin, MSBFIRST, rows);
+      //digitalWrite(clockPin, LOW);
+      SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+      SPI.transfer(rv.vals,3);
+      SPI.endTransaction();
+#if 0
+      shiftOut(dataPin, clockPin, MSBFIRST, rv.greenCol);
+      shiftOut(dataPin, clockPin, MSBFIRST, rv.redCol);
+      shiftOut(dataPin, clockPin, MSBFIRST, rv.row);
+#endif
       digitalWrite(latchPin, HIGH);
       delay(50);
     }
