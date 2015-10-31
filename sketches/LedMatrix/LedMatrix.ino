9d77cb0ed2ac9212a0bb908d0e38d4a01bc40e01
sibosop <brian@eastshore.com>
2015-10-30 21:38:11 -0700

    checkpoint wiring corrected

--- sketches/LedMatrix/LedMatrix.ino
+++ sketches/LedMatrix/LedMatrix.ino
@@ -8,20 +8,42 @@ const int resetPin = 5;
 
 byte red;
 byte green;
+byte drow;
 void patternCallback(Task* task) {
   LedMatrix.clear();
 #if 0
   if ( ++red == 8 )
     red = 0;
-  green = 7 - red;
+  green = red;
   for (int i = 0; i < 8; i++ )
   {  
-    LedMatrix.set(i,red,LedMatrixClass::RedPos);
-    LedMatrix.set(i,green,LedMatrixClass::GreenPos);
+    Serial.print("red:"); Serial.print(red,HEX);
+    Serial.print(" green:"); Serial.print(green,HEX);
+    Serial.println();
+    if ( i % 2 )
+      LedMatrix.set(i,red,LedMatrixClass::RedPos);
+    else
+      LedMatrix.set(i,green,LedMatrixClass::GreenPos);
   }
-#else
+#endif
+#if 0
+  if ( ++red == 8 )
+    red = 0;
+  green = 7 - red;
+  drow = red;
+  
+  
+  Serial.print("drow:"); Serial.print(drow,HEX);
+  Serial.print(" red:"); Serial.print(red,HEX);
+  Serial.print(" green:"); Serial.println(green,HEX);
+  LedMatrix.set(drow,red,LedMatrixClass::RedPos);
+  LedMatrix.set(drow,green,LedMatrixClass::GreenPos);
+  
+#endif
+
+#if 1
   for (int i = 0; i < 8; i++ )
-  {  
+  { 
     red = random(8);
     green = random(8);
     LedMatrix.set(i,red,LedMatrixClass::RedPos);
@@ -29,7 +51,7 @@ void patternCallback(Task* task) {
   }
 #endif
 }
-Task patternTimer(1000,patternCallback);
+Task patternTimer(100,patternCallback);
 
 byte row;
 void refreshCallback(Task* task) {
@@ -48,5 +70,6 @@ void setup() {
   SoftTimer.add(&patternTimer);
   red = 0;
   green = 0;
+  drow = 0;
   randomSeed(analogRead(0));
 }
