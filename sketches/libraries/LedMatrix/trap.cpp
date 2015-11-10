23f41de099347591c83a7ce7ad42aa487845fd34
sibosop <brian@eastshore.com>
2015-11-09 20:26:39 -0800

    rotating trap

--- sketches/libraries/LedMatrix/trap.cpp
+++ sketches/libraries/LedMatrix/trap.cpp
@@ -1,33 +1,45 @@
 #include "LedMatrix.h"
 #include "trap.h"
 
-const Pos Trap::positions[4] =
+const Pos Trap::positions[] =
 {
   Pos(3,3)
   ,Pos(4,4)
   ,Pos(5,3)
   ,Pos(4,2)
+  ,Pos(3,4)
+  ,Pos(5,4)
+  ,Pos(5,2)
+  ,Pos(3,2)
 };
 
 const Color Trap::colors[4] =
 {
   Color(0x10,0)
-  ,Color(0x1f,0x1f)
   ,Color(0x0,0x1f)
-  ,Color(0x10,0x1f)
+  ,Color(0x1f,0x1f)
+  ,Color(0x5,0x1f)
 };
 
 void
 Trap::display()
 {
-  if ( !(count++ % 20) )
-    ++swap;
+  if ( !(count++ % 2) )
+  {
+    if ( !offset )
+      offset = 4;
+    else
+    {
+      swap += 3;
+      offset = 0;
+    }
+  }
   
   for (int i = 0; i < 4; ++i)
   {
     Pixel p;
-    p.p = positions[i];
-    p.c = colors[(i+swap) % 4];
+    p.p = positions[i+offset];
+    p.c = colors[(i+swap)%4];
     LedMatrix.setPixel(p);
   }
 }
--- sketches/libraries/LedMatrix/trap.h
+++ sketches/libraries/LedMatrix/trap.h
@@ -5,12 +5,14 @@ public:
   Trap() 
     : count(0)
     , swap(0) 
+    , offset(0)
     {}
   void display();
 private:
-  static const Pos positions[4];
-  static const Color colors[4];
+  static const Pos positions[];
+  static const Color colors[];
   unsigned int count;
   unsigned int swap;
+  unsigned int offset;
 };
 #endif
