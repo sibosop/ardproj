c5c89d099cb4c9934f61b3c1c365ba9cb1038fed
sibosop <brian@eastshore.com>
2018-03-09 14:53:31 -0800

    added pulse timer to goodBad, added offset setup to NeoClock.

--- sketches/GoodBadMeter/GoodBadMeter.ino
+++ sketches/GoodBadMeter/GoodBadMeter.ino
@@ -6,6 +6,7 @@ const int ValuePin = 3;
 const int Dir1Pin = 4;
 const int Dir2Pin = 5;
 const int ChoiceButtonPin = 6;
+const int LedPin = 9;
 
 #define FAST_TIME 50
 #define CHOICE_TIME 1000
@@ -54,20 +55,46 @@ buttonReaderCallback(Task* task) {
   }
 }
 
+uint8_t pulseVal;
+bool  pulseUp;
+const uint8_t pulseMax = 150;
+const uint8_t pulseMin = 10;
+void
+ledPulser(Task *task) {
+  if (choiceTimer)
+  {
+    pulseVal = 0;
+    pulseUp  = true;
+  } else if (pulseUp) {
+    ++pulseVal;
+    if (pulseVal == pulseMax)
+      pulseUp = false;
+  } else {
+    if ( --pulseVal == pulseMin )
+      pulseUp = true;
+  }
+  //Serial.println(pulseVal,DEC);
+  analogWrite(LedPin,pulseVal);
+}
 
 
 Task buttonTimer(3,buttonReaderCallback);
+Task ledTimer(10,ledPulser);
 
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(A4));
   buttTimer = FAST_TIME;
   choiceTimer = 0;
+  pulseVal = 0;
+  pulseUp = true;
   
   pinMode(ValuePin,OUTPUT);
   pinMode(Dir1Pin,OUTPUT);
   pinMode(Dir2Pin,OUTPUT);
+  pinMode(LedPin,OUTPUT);
   pinMode(ChoiceButtonPin,INPUT_PULLUP);
   
   SoftTimer.add(&buttonTimer);
+  SoftTimer.add(&ledTimer);
 }
