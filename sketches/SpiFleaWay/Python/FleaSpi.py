31efd030aa9842b019eabc1b01e9153707eb45f1
brian reinbolt <brian@eastshore.com>
2018-06-08 18:27:00 -0700

    commented out spi code

--- sketches/SpiFleaWay/Python/FleaSpi.py
+++ sketches/SpiFleaWay/Python/FleaSpi.py
@@ -12,12 +12,13 @@ def isMac():
   return rval
 
 if not isMac():
-  import spidev
-  spi = spidev.SpiDev()
-  spi.open(0,1)
-  spi.max_speed_hz = 1000000
-  print spi.max_speed_hz
-  print spi.lsbfirst
+  if False:
+    import spidev
+    spi = spidev.SpiDev()
+    spi.open(0,1)
+    spi.max_speed_hz = 1000000
+    print spi.max_speed_hz
+    print spi.lsbfirst
 
 
 buff = ""
@@ -32,19 +33,20 @@ def flushIt():
   if isMac():
     if debug: print buff.rstrip()
   else:
-    val = 0
-    try:
-      while True:
-        buf = "%08X"%val+"\n";
-        o = []
-        for c in buf:
-          o.append(ord(c))
-          #print o
-        resp = spi.writebytes(o)
-        val += 1
-        time.sleep(.0001)
-    except KeyboardInterrupt:
-      spi.close()
+    if False:
+      val = 0
+      try:
+        while True:
+          buf = "%08X"%val+"\n";
+          o = []
+          for c in buf:
+            o.append(ord(c))
+            #print o
+          resp = spi.writebytes(o)
+          val += 1
+          time.sleep(.0001)
+      except KeyboardInterrupt:
+        spi.close()
   buff = ""
   
   
